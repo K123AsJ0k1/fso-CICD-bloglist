8c945fb60ea0f4ae88b086c583b1de5607a5730e
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-08-18 11:25:57 +0300

    Test

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use('/api/login', loginRouter)
 // eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === 'test') {
   console.log('test')
+  // eslint-disable-next-line no-undef
   console.log(`Used port:${process.env.PORT}`)
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
